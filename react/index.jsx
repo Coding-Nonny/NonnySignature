d86175b306d141d1121635047198fe6db792d2e2
Coding-Nonny <chikezienonny@gmail.com>
2023-04-21 17:13:09 +0100

    adding usage guidelines

--- react/index.jsx
+++ react/index.jsx
@@ -6,7 +6,6 @@ export default function SignaturePad({id, children, onSave }) {
     const signaturePad = new NonnySignature(`#${id}`, children?? false);
     signaturePad.onSave(onSave);
     console.log(document.querySelector(`#${id}`));
-    //signaturePad.watch();
   },[]);
 
   return <div id={id}>
